919e1ed2ba45b0e18c7522661b448cf8e51072c0
Chesterkxng <cgoita00@gmail.com>
2024-07-11 20:46:12 +0000

    om model

---
+++ src/model/om.php
@@ -0,0 +1,129 @@
+<?php
+namespace Application\Model\OM;
+
+require_once ('src/lib/database.php');
+use Application\Lib\Database\DatabaseConnection;
+
+class OM
+{
+    public int $om_id;
+    public string $recipient;
+    public string $city;
+    public string $companions;
+    public string $object;
+    public string $means;
+    public string $departure_date;
+    public string $return_date;
+    public string $edition_date;
+    public string $url;
+}
+class OMRepository
+{
+    public DatabaseConnection $connection;
+
+    public function addOM(
+        string $recipient,
+        string $city,
+        string $companions,
+        string $object,
+        string $means,
+        string $departure_date,
+        string $return_date,
+    ): bool {
+
+        $statement = $this->connection->getConnection()->prepare(
+            "INSERT INTO `om`(`RECIPIENT`, `CITY`, `COMPANIONS`, `OBJECT`, `MEANS`, `DEPARTURE_DATE`, `RETURN_DATE`,`EDITION_DATE`) 
+            VALUES (?,?,?,?,?,?,?,?)"
+        );
+        $edition_date = date('Y-m-d');
+        $statement->execute([$recipient, $city, $companions, $object, $means, $departure_date, $return_date, $edition_date]);
+        $affectedLines = $statement->rowCount();
+        return $affectedLines == 1;
+
+    }
+
+    public function getOMs(): array
+    {
+        $statement = $this->connection->getConnection()->query(
+            "SELECT * FROM `om`"
+        );
+        $OMs = [];
+        while ($row = $statement->fetch()) {
+            $OM = new OM();
+            $OM->om_id = $row["OM_ID"];
+            $OM->recipient = $row["RECIPIENT"];
+            $OM->city = $row["CITY"];
+            $OM->companions = $row["COMPANIONS"];
+            $OM->object = $row["OBJECT"];
+            $OM->means = $row["MEANS"];
+            $OM->departure_date = $row["DEPARTURE_DATE"];
+            $OM->return_date = $row["RETURN_DATE"];
+            $OM->edition_date = $row["EDITION_DATE"];
+
+            $OMs[] = $OM;
+        }
+        return $OMs;
+    }
+    public function getOM(int $om_id): OM
+    {
+        $statement = $this->connection->getConnection()->prepare(
+            "SELECT * FROM `om` 
+            WHERE `OM_ID` = ? "
+        );
+        $statement->execute([$om_id]);
+        while ($row = $statement->fetch()) {
+            $OM = new OM();
+            $OM->om_id = $row["OM_ID"];
+            $OM->recipient = $row["RECIPIENT"];
+            $OM->city = $row["CITY"];
+            $OM->companions = $row["COMPANIONS"];
+            $OM->object = $row["OBJECT"];
+            $OM->means = $row["MEANS"];
+            $OM->departure_date = $row["DEPARTURE_DATE"];
+            $OM->return_date = $row["RETURN_DATE"];
+            $OM->edition_date = $row["EDITION_DATE"];
+            $OM->url = $row["URL"];
+        }
+        return $OM;
+    }
+    public function getIntOMs()
+    {
+        $statement = $this->connection->getConnection()->query(
+            "SELECT * FROM `om`
+            ORDER BY `EDITION_DATE` DESC"
+        );
+        $OMs = [];
+        while ($row = $statement->fetch()) {
+            $OM = new OM();
+            $OM->om_id = $row["OM_ID"];
+            $OM->recipient = $row["RECIPIENT"];
+            $OM->city = $row["CITY"];
+            $OM->companions = $row["COMPANIONS"];
+            $OM->object = $row["OBJECT"];
+            $OM->means = $row["MEANS"];
+            $OM->departure_date = $row["DEPARTURE_DATE"];
+            $OM->return_date = $row["RETURN_DATE"];
+            $OM->edition_date = $row["EDITION_DATE"];
+            $OM->url = $row["URL"];
+
+            $OMs[] = $OM;
+        }
+        return $OMs;
+
+    }
+
+    public function uploadMO(string $url, int $om_id): bool
+    {
+
+        $statement = $this->connection->getConnection()->prepare(
+            "UPDATE `om`
+            SET `URL` = ? 
+            WHERE `OM_ID` = ?"
+        );
+        $statement->execute([$url, $om_id]);
+        $affectedLines = $statement->rowCount();
+        return $affectedLines == 1;
+
+    }
+
+}
